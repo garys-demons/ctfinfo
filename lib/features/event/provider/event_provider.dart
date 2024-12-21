c6dec2a9bbc9ab85870257b67bfffe4e5dadf514
Roshan Kumar <roshaen09@gmail.com>
2024-12-21 12:35:51 +0530

    [feature] Added event details provider

--- lib/features/event/provider/event_provider.dart
+++ lib/features/event/provider/event_provider.dart
@@ -1,3 +1,4 @@
+import 'package:ctfinfo/features/event/models/event_detail_model.dart';
 import 'package:ctfinfo/features/event/models/event_model.dart';
 import 'package:ctfinfo/features/event/repository/event_repository.dart';
 import 'package:flutter/material.dart';
@@ -6,7 +7,9 @@ class EventProvider extends ChangeNotifier {
   final _repository = EventRepository();
 
   final List<EventModel> _events = [];
+  EventDetailModel _eventDetail = EventDetailModel();
   List<EventModel> get events => _events;
+  EventDetailModel get eventDetail => _eventDetail;
 
   Future<bool> getAllEvents() async {
     try {
@@ -19,6 +22,19 @@ class EventProvider extends ChangeNotifier {
       return true;
     } catch (e) {
       debugPrint("Error in getting events: $e");
+      notifyListeners();
+      return false;
+    }
+  }
+
+  Future<bool> getEventDetail(String eventId) async {
+    try {
+      final response = await _repository.getEventDetail(eventId);
+      _eventDetail = EventDetailModel.fromJson(response);
+      notifyListeners();
+      return true;
+    } catch (e) {
+      debugPrint("Error in getting event detail: $e");
       notifyListeners();
       return false;
     }
