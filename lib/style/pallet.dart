204d46a4247a2b392ec71a7d8dafdcc2e2e19276
Gary <178498812+garys-demons@users.noreply.github.com>
2025-02-25 01:12:14 +0530

    [ui] edited theme and pallet file

--- lib/style/pallet.dart
+++ lib/style/pallet.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 extension Pallet on ColorScheme {
   static const blackColour = Color(0xFF0A0A0A);
   static const greenColour = Color(0xFF5DFF5A);
+  static const errorColour = Color(0xFFFF3235);
 }
